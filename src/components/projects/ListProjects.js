5a80f6c4b829105abce4caafd3e4244ae6946c86
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-10-01 11:36:06 +0200

    calling projects with dispatch

--- src/components/projects/ListProjects.js
+++ src/components/projects/ListProjects.js
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import projectContext from '../../context/projects/projectContex';
 import Project from './Project';
 
 const ListProjects = () => {
   const projectsContext = useContext(projectContext);
-  const { projects } = projectsContext;
+  const { projects, getProjects } = projectsContext;
+
+  useEffect(() => {
+    getProjects();
+  }, []);
 
   if (projects.length === 0) return null;
+
   return (
     <ul className="list-projects">
       {projects.map((project) => (
--- src/context/projects/projectState.js
+++ src/context/projects/projectState.js
@@ -1,16 +1,17 @@
 import React, { useReducer } from 'react';
-import { FORM_PROJECT } from '../../types';
+import { FORM_PROJECT, GET_PROJECTS } from '../../types';
 import projectContext from './projectContex';
 import projectReducer from './projectReducer';
 
 const ProjectState = (props) => {
+  const projects = [
+    { id: 1, name: 'Online shop' },
+    { id: 2, name: 'intranet' },
+    { id: 3, name: 'website' },
+  ];
   const initialState = {
     form: false,
-    projects: [
-      { id: 1, name: 'Online shop' },
-      { id: 2, name: 'intranet' },
-      { id: 3, name: 'website' },
-    ],
+    projects: [],
   };
   const [state, dispatch] = useReducer(projectReducer, initialState);
 
@@ -20,9 +21,21 @@ const ProjectState = (props) => {
     });
   };
 
+  const getProjects = () => {
+    dispatch({
+      type: GET_PROJECTS,
+      payload: projects,
+    });
+  };
+
   return (
     <projectContext.Provider
-      value={{ form: state.form, projects: state.projects, showForm }}
+      value={{
+        form: state.form,
+        projects: state.projects,
+        showForm,
+        getProjects,
+      }}
     >
       {props.children}
     </projectContext.Provider>
